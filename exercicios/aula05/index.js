aa853152418cc9d8a606fada43e30cdee733d962
mhsr-rgb <mhsr@aluno.ifnmg.edu.br>
2026-04-09 18:43:10 +0000

    feat: pratica funções e métodos de array e pratica fundamentos de JavaScript no Node - aula 05

--- exercicios/aula05/index.js
+++ exercicios/aula05/index.js
@@ -8,24 +8,57 @@
 // Exercício 1 — classificarIdade
 export function classificarIdade(idade) {
   // escreva seu código aqui
+  if(idade < 12){
+    return "criança";
+  }
+  else if(idade < 18){
+    return "adolescente"
+  }
+  else {
+    return "adulto"
+  }
 }
 
 // Exercício 2 — calcularDesconto
 export function calcularDesconto(preco, percentual) {
   // escreva seu código aqui
+  return preco - ((percentual / 100) * preco);
 }
 
 // Exercício 3 — verificarAprovacao
 export function verificarAprovacao(nota) {
   // escreva seu código aqui
+  if(nota >= 60){
+    return "aprovado";
+  }
+  else if(nota >= 40){
+    return "recuperação";
+  } 
+  else{
+    return "reprovado";
+  }
 }
 
 // Exercício 4 — somarPares
 export function somarPares(numeros) {
   // escreva seu código aqui
+  let soma = 0;
+  for(const numero of numeros){
+    if(numero % 2 === 0){
+      soma += numero;
+    }
+  }
+  return soma;
 }
 
 // Exercício 5 — contarLetra
 export function contarLetra(texto, letra) {
   // escreva seu código aqui
+  let numLetra = 0;
+  for(const carracter of texto){
+    if(carracter === letra){
+      numLetra += 1;
+    }
+  }
+  return numLetra;
 }
--- exercicios/aula06/index.js
+++ exercicios/aula06/index.js
@@ -8,24 +8,39 @@
 // Exercício 1 — criarSaudacao
 export function criarSaudacao(nome) {
   // escreva seu código aqui
+  return `Olá, ${nome}!`;
 }
 
 // Exercício 2 — filtrarAprovados
 export function filtrarAprovados(alunos) {
   // escreva seu código aqui
+  const aprovados = alunos.filter((aluno) => aluno.nota >= 60);
+  return aprovados;
 }
 
 // Exercício 3 — extrairNomes
 export function extrairNomes(alunos) {
   // escreva seu código aqui
+  const nomes = alunos.map((aluno) => aluno.nome);
+  return nomes;
 }
 
 // Exercício 4 — buscarAluno
 export function buscarAluno(alunos, nome) {
   // escreva seu código aqui
+  const pessoa = alunos.find((aluno) => aluno.nome === nome);
+  return pessoa;
 }
 
 // Exercício 5 — calcularMedia
 export function calcularMedia(notas) {
   // escreva seu código aqui
+  let mediaAritimetica = 0;
+  if(notas.length === 0){
+    return 0;
+  }
+  for(const nota of notas){
+    mediaAritimetica += nota;
+  }
+  return mediaAritimetica / notas.length;
 }
